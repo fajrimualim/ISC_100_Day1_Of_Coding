c561ac8f38d74b1b50b186bc8cb4b088db6811c7
fajrimualim <147229764+fajrimualim@users.noreply.github.com>
2023-10-09 17:01:51 +0700

    Add files via upload

---
+++ Day1_biodata.java
@@ -0,0 +1,22 @@
+
+package FAJRI;
+
+
+public class Day1_biodata {
+    public static void main(String[] args) {
+        //Data biodata
+        String nama = "m fajri mualim";
+        String nim = "D0222372";
+        String kelas = "informatika E";
+        int angkatan = 2022;
+        
+        //Menampilkan biodata
+        System.out.println("biodata");
+        System.out.println("nama:"+ nama);
+        System.out.println("nim"+ nim);
+        System.out.println("kelas"+ kelas);
+        System.out.println("angkatan"+ angkatan);
+        
+    }
+    
+}
